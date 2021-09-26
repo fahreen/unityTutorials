428379f58e35306d94df042c144f30727c73657a
fahreen <fahreen.b@gmail.com>
2021-09-25 21:38:19 -0400

    cube demo

---
+++ Personal Project/Assets/ModTheCube/Cube.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    public MeshRenderer Renderer;
+    
+    void Start()
+    {
+        transform.position = new Vector3(3, 6, 1);
+
+        float scale = Random.Range(1.0f, 20.0f);
+        transform.localScale = Vector3.one * scale;
+        
+        Material material = Renderer.material;
+        
+        material.color = new Color(1,0,1,1);
+    }
+    
+    void Update()
+    {
+
+        transform.Rotate(-10.0f * Time.deltaTime*20, 0.0f, 0.0f);
+    }
+}
